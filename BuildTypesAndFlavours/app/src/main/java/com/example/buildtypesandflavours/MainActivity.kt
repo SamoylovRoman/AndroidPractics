00df4c30164136a893f664c88fe2706352c97ab6
Roman Samoylov <samoylov.r.v@gmail.com>
2022-02-18 14:55:21 +0700

    Build types and flavours init commit

--- BuildTypesAndFlavours/app/src/main/java/com/example/buildtypesandflavours/MainActivity.kt
+++ BuildTypesAndFlavours/app/src/main/java/com/example/buildtypesandflavours/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.buildtypesandflavours
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.buildtypesandflavours.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textInformation.text = """
+    Flavor = ${BuildConfig.FLAVOR}
+    Build type = ${BuildConfig.BUILD_TYPE}
+    Version code = ${BuildConfig.VERSION_CODE}
+    Version name = ${BuildConfig.VERSION_NAME}
+""".trimIndent()
     }
 }
