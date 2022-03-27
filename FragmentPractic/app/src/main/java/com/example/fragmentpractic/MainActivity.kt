38260f2d33f5658b84f0f640e35930e774ea66f1
Roman Samoylov <samoylov.r.v@gmail.com>
2022-03-27 20:12:19 +0700

    Fragment practice commit

---
+++ FragmentPractic/app/src/main/java/com/example/fragmentpractic/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.fragmentpractic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.commit {
+            replace<BlankFragment>(R.id.fragment_container)
+            addToBackStack(BlankFragment::class.java.simpleName)
+        }
+    }
+}
