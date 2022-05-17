feba1298a587fe633673b07463841ce33cb649bd
Roman Samoylov <samoylov.r.v@gmail.com>
2022-05-17 16:45:33 +0700

    ViewModelAndNavigation additional commit 2 for review

--- ViewModelAndNavigation/app/src/main/java/com/example/viewmodelandnavigation/StaffListFragment.kt
+++ ViewModelAndNavigation/app/src/main/java/com/example/viewmodelandnavigation/StaffListFragment.kt
@@ -1,14 +1,11 @@
 package com.example.viewmodelandnavigation
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.os.HandlerCompat.postDelayed
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.*
