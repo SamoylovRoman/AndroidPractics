58b55e1691b1685f51d19e0090f8a410b14d0811
Roman Samoylov <samoylov.r.v@gmail.com>
2022-07-02 10:22:43 +0700

    Custom View training commit GantView.kt

---
+++ AddToCartCustomButtonView/app/src/main/java/com/codelabs/addtocartcustombuttonview/AddToCardCustomButtonView.kt
@@ -0,0 +1,10 @@
+package com.codelabs.addtocartcustombuttonview
+
+import android.content.Context
+import android.view.ViewGroup
+import android.widget.LinearLayout
+
+/*
+class AddToCardCustomButtonView : LinearLayout() {
+
+}*/
